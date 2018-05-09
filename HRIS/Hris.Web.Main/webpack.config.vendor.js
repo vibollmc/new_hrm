0cb53c5ff4e36157c1a708b91d47ee142b47f07d
Cuong Bui <cuongvnm.dh@gmail.com>
2018-05-09 15:17:09 +0700

    init 4

--- HRIS/Hris.Web.Main/webpack.config.vendor.js
+++ HRIS/Hris.Web.Main/webpack.config.vendor.js
@@ -12,15 +12,12 @@ const treeShakableModules = [
     '@angular/platform-browser',
     '@angular/platform-browser-dynamic',
     '@angular/router',
-    'zone.js',
+    'zone.js'
 ];
 const nonTreeShakableModules = [
-    'bootstrap',
-    'bootstrap/dist/css/bootstrap.css',
     'es6-promise',
     'es6-shim',
-    'event-source-polyfill',
-    'jquery',
+    'event-source-polyfill'
 ];
 const allModules = treeShakableModules.concat(nonTreeShakableModules);
 
